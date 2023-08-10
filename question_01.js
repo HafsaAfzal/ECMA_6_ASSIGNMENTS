9013b2e11f153e2d9af9f97c2dcb395e5d0ee877
HafsaAfzal <hafsaafzal380@gmail.com>
2023-08-10 09:31:56 +0500

    initial commit

---
+++ question_01.js
@@ -0,0 +1,2 @@
+// Write a function that takes two numbers and returns their sum. Use default parameters
+// to assign 0 as the default value for both numbers.
---
+++ question_02.js
@@ -0,0 +1,6 @@
+// Write a function that takes any number of arguments and returns an array with all the
+// arguments. Use rest parameters to collect the arguments into an array.
+function printArray(...rest){
+    return(rest);
+}
+const result=printArray(7,5,8,9,2,0,1);
