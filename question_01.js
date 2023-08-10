1915dd9bf1b9147b2a215ffd7029b869be89ed9b
HafsaAfzal <hafsaafzal380@gmail.com>
2023-08-10 10:31:18 +0500

    qestion#01 code added

--- question_01.js
+++ question_01.js
@@ -1,2 +1,7 @@
 // Write a function that takes two numbers and returns their sum. Use default parameters
 // to assign 0 as the default value for both numbers.
+function add(num1=0,num2=0){
+return (num1+num2);
+}
+result = add();
+console.log(result);
